533963de22ce77cd743852b288c0508fe6f0158b
Tim <Tim@MacBook-Pro-de-Tim.local>
2020-01-28 14:28:22 +0100

    new router for posts and comments

---
+++ routes/comments.js
@@ -0,0 +1,12 @@
+// routes/comments.js
+const express = require('express');
+const fakeComments = require('../data/comments');
+const router = express.Router();
+
+
+// Get a list of comments
+router.get('/', (req, res) => {
+    res.json(fakeComments);
+});
+
+module.exports = router;
