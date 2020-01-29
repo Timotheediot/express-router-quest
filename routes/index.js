aa864f23889288c73592ac357b90f97d23c95790
Tim <Tim@MacBook-Pro-de-Tim.local>
2020-01-29 09:21:27 +0100

    new tags route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const comments = require('./comments')
 const posts = require('./posts');
+const tags = require('./tags')
 
 const router = express.Router();
 
 router.use('/posts', posts);
 router.use('/comments', comments);
+router.use('/tags', tags)
+
 
 module.exports = router;
---
+++ routes/tags.js
@@ -0,0 +1,13 @@
+// routes/tags.js
+const express = require('express');
+const tags = require('../data/tags')
+const router = express.Router();
+
+
+
+// Get a list of tags
+router.get('/', (req, res) => {
+    res.json(tags);
+});
+
+module.exports = router;
